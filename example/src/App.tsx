7df3b56232f3540236267e1bee4b947f97f666e4
Nimantha Nadeeraka <info.nadeeraka@gmail.com>
2020-07-27 00:11:05 +0530

    modification add

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { ExampleComponent } from 'password-meter'
+import {ExampleComponent } from 'password-meter'
 import 'password-meter/dist/index.css'
 
 const App = () => {
